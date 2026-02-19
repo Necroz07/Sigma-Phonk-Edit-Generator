45e11ec24519a5838b5e7da9e22f8dd7172b3a76
Shrey Shah <rizzcularsuccess@gmail.com>
2026-02-19 23:00:46 +0530

    attempt to add smile logic

--- newlogic.py
+++ newlogic.py
@@ -1,8 +1,32 @@
 import cv2
+import math
+
 import mediapipe as medpi
 
 from mediapipe.tasks.python import vision
 from mediapipe.tasks.python import BaseOptions
+
+
+
+def distance(a, b):
+    distx = a.x - b.x
+    disty = a.y - b.y
+
+    return math.hypot(distx, disty)
+
+
+def smilescore(lms):
+
+    left_corner = 61
+    right_corner = 291
+    upperlip = 13
+    lowerlip = 14
+    
+    w = distance(lms[left_corner], lms[right_corner])
+    h = distance(lms[upperlip], lms[lowerlip])
+
+    mar = w/(h + 1e-6)
+    return mar
 
 baseopt = BaseOptions(model_asset_path="face_landmarker.task")
 options = vision.FaceLandmarkerOptions(base_options = baseopt, num_faces = 1)
@@ -22,10 +46,23 @@ while True:
 
     result = detect.detect(mp_img)
 
+    '''
     if result.face_landmarks:
         cv2.putText(frame, "FACE DETECTED", (99, 150), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 3)
 
-        print(f"Landmarks= {len(result.face_landmarks[0])}")
+        print(f"Landmarks= {len(result.face_landmarks[0])}") '''
+
+    lms = result.face_landmarks[0]
+    score = smilescore(lms)
+
+    if score > 18.0:
+        cv2.putText(frame, "SMILE DETECTED", (99, 150), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 3)
+
+    else:
+        cv2.putText(frame, "NO SMILE DETECTED", (99, 150), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 3)
+
+
+
 
     cv2.imshow("Webcam", frame)
 
