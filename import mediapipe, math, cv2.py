b0b7f6ab11f4ab422de3a088e8426c90e05a677b
Shrey Shah <rizzcularsuccess@gmail.com>
2026-02-22 23:04:28 +0530

    ....

---
+++ import mediapipe, math, cv2.py
@@ -0,0 +1,61 @@
+import mediapipe, math, cv2
+from mediapipe.tasks.python import BaseOptions
+from mediapipe.tasks.python import vision
+import mediapipe as medpi
+
+def distance(a,b):
+    return math.hypot(a.x - b.x, a.y, b.y)
+
+def smilescore(lms):
+
+    left_corner=61
+    right_corner=291
+    chin=152
+    forehead=10
+    upper_lip=13
+
+    avgycorner = (lms[left_corner].y + lms[right_corner].y)/2
+    faceheight = abs(lms[chin].y - lms[forehead].y)
+
+    lift = lms[upper_lip].y - avgycorner
+
+    normalizedlift = lift/faceheight
+
+    return normalizedlift
+
+baseopt = BaseOptions(model_asset_path='fase_landmarker.task')
+options = vision.FaceLandmarkerOptions(base_options=baseopt, num_faces=1)
+detect = vision.FaceLandmarker.create_from_options(options)
+
+cap = cv2.VideoCapture(3)
+
+smooted = None
+alpha = 0.2
+
+if not cap.isOpened:
+    raise RuntimeError("can't open yo cam bro")
+
+while True: 
+    ok, frame = cap.read()
+    
+    if not ok:
+        break
+
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    mp_img = medpi.Image(image_format=medpi.ImageFormat, data=rgb)
+    result = detect.detect(mp_img)
+
+    if not result.face_landmarks:
+        cv2.putText(frame, "No Face", (99, 150), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+        cv2.imshow("CAM", frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+        continue
+
+    lms = result.face_landmarks[0]
+
+    
+
+
+
+
