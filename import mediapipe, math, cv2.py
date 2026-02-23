c2bfcfb2ba5c0b06490c0a6a29a39df825c89fb1
Shrey Shah <rizzcularsuccess@gmail.com>
2026-02-23 21:39:50 +0530

    trying to implement freeze logic

--- import mediapipe, math, cv2.py
+++ import mediapipe, math, cv2.py
@@ -1,10 +1,6 @@
-import mediapipe, math, cv2
 from mediapipe.tasks.python import BaseOptions
 from mediapipe.tasks.python import vision
-import mediapipe as medpi
-
-def distance(a,b):
-    return math.hypot(a.x - b.x, a.y, b.y)
+import time, cv2, mediapipe as medpi
 
 def smilescore(lms):
 
@@ -23,16 +19,18 @@ def smilescore(lms):
 
     return normalizedlift
 
-baseopt = BaseOptions(model_asset_path='fase_landmarker.task')
+baseopt = BaseOptions(model_asset_path='face_landmarker.task')
 options = vision.FaceLandmarkerOptions(base_options=baseopt, num_faces=1)
 detect = vision.FaceLandmarker.create_from_options(options)
 
 cap = cv2.VideoCapture(3)
 
-smooted = None
-alpha = 0.2
+currenttime = None
+waittime = 3
 
-if not cap.isOpened:
+thresh= -0.0001
+
+if not cap.isOpened():
     raise RuntimeError("can't open yo cam bro")
 
 while True: 
@@ -42,20 +40,42 @@ while True:
         break
 
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    mp_img = medpi.Image(image_format=medpi.ImageFormat, data=rgb)
+    mp_img = medpi.Image(image_format=medpi.ImageFormat.SRGB, data=rgb)
     result = detect.detect(mp_img)
 
     if not result.face_landmarks:
         cv2.putText(frame, "No Face", (99, 150), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
         cv2.imshow("CAM", frame)
+
         if cv2.waitKey(1) == ord('q'):
             break
         continue
 
     lms = result.face_landmarks[0]
 
+    score = smilescore(lms)
+
+    if score > thresh:
+        if currenttime is None:
+            currenttime = time.time()
+
+        if (time.time() - currenttime) > waittime:
+
+            cv2.putText(frame, f"SMILE DETECTED {score}", (99, 150), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 3)
+            cv2.imshow("CAM", frame)
+        
+            if cv2.waitKey(500) == ord('q'):
+                break
+
+    else:
+        cv2.imshow("CAM", frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+        currenttime= None
+
+
+    if cv2.waitKey(1) == ord('q'):
+            break
     
-
-
-
-
+cap.release()
+cv2.destroyAllWindows()
